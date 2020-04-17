846a13db4729cc4a978a3f29ff882dc94a4d6824
Ben Highsted <49290301+BenHighsted@users.noreply.github.com>
2020-04-17 17:33:29 +1200

    Removed some redundant code

--- ComputerInfoUI.py
+++ ComputerInfoUI.py
@@ -10,12 +10,14 @@ import platform
 import os
 
 import time
+import string
+
+#At the moment just want to get all the data working in a terminal app, then convert it into a UI.
 
 def run():
     print("Running...\n")
-    print("CPU Percent:", psutil.cpu_percent())
-    print(psutil.virtual_memory())
-    memory = psutil.virtual_memory() #becomes an array containing memory information
+    calculate_cpu_usage()
+    calculate_memory_usage()
 
     print()
 
@@ -23,24 +25,21 @@ def run():
 
     i = 0
     while True:
-        process = psutil.Process(os.getpid())
-        print("Memory Usage: " + str(round(process.memory_percent() * 100, 2)) + "%")
+        calculate_memory_usage()
+        calculate_cpu_usage()
         time.sleep(5)
+        #below will stop the code. Once the GUI is developed it should run without stopping (unless told to by the user)
         if i == 10:
             break
         i += 1
-    
-"""
+        
 
-For windows https://stackoverflow.com/questions/938733/total-memory-used-by-python-process.
-Will need to install WMI on my windows computer.
+def calculate_memory_usage():
+    memory = psutil.virtual_memory() #becomes an array containing memory information
+    print("Memory Usage: " + str(memory[2]) + "%")
 
-def memory():
-    from wmi import WMI
-    w = WMI('.')
-    result = w.query("SELECT WorkingSet FROM Win32_PerfRawData_PerfProc_Process WHERE IDProcess=%d" % os.getpid())
-    return int(result[0].WorkingSet)
-
-"""
+def calculate_cpu_usage():
+    process = psutil.Process(os.getpid())
+    print(process.cpu_percent())
 
 run()
